1ff4fd8d73b1cefaac188c9d0a784afb6facdcbd
Devansh-v2 <devanshshukla5737@gmail.com>
2024-03-18 17:04:48 +0530

    Adding Streamlit Functions

--- test_enhance_image.py
+++ test_enhance_image.py
@@ -1,25 +1,140 @@
-# enhance_image_app.py
+
 import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
-from image_enhance import enhance_image
+import image_enhance as ie
 
 # from util import set_background
 
 # set_background('pix/1.png')
+def original(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
 
-def enhance_uploaded_image(uploaded_image):
+        # Enhance the image
+        enhanced_image = ie.original(img)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Original Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+
+def brightness(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.brightness(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Brightend Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+def contrast(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.contrast(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Darkend Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+
+def gamma(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.gamma_correction(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Gamma Corrected Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+def edge(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.edge_detector(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Edges Detected', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+
+
+def motion(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.reduce_motion_blur(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Sharpened Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+def skin(uploaded_image):
     try:
         # Convert uploaded image to OpenCV format
         img = Image.open(uploaded_image)
         # img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
         # Enhance the image
-        enhanced_image = enhance_image(img)
+        enhanced_image = ie.skin_detection(img)
 
         # Display the enhanced image
-        st.image(enhanced_image, caption='Enhanced Image', use_column_width=True)
+        st.image(enhanced_image, caption='Skin Detected Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+
+def custom(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.Image_Enchancer(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Enchanced Image', use_column_width=True)
+    except Exception as e:
+        st.error("Error enhancing image: {}".format(str(e)))
+
+def resize(uploaded_image):
+    try:
+        # Convert uploaded image to OpenCV format
+        img = Image.open(uploaded_image)
+        img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+
+        # Enhance the image
+        enhanced_image = ie.image_resizer(img_cv)
+
+        # Display the enhanced image
+        st.image(enhanced_image, caption='Resized Image', use_column_width=True)
     except Exception as e:
         st.error("Error enhancing image: {}".format(str(e)))
 
@@ -30,7 +145,35 @@ def main():
     uploaded_image = st.file_uploader("Upload Image", type=['jpg', 'jpeg'])
 
     if uploaded_image is not None:
-        enhance_uploaded_image(uploaded_image)
+        operation = st.selectbox('Choose an operation',['Original','Auto Enhancer','Bright', 'Contrast', 'Gamma Correction', 'Sharpness','Skin Detector', 'Edge Detector','Resize'])
+
+        if operation == 'Bright':
+            brightness(uploaded_image)
+
+        elif operation == 'Contrast':
+            contrast(uploaded_image)
+        
+        elif operation == 'Auto Enhancer':
+            custom(uploaded_image)
+
+        elif operation == 'Gamma Correction':
+            gamma(uploaded_image)
+
+        elif operation == 'Sharpness':
+            motion(uploaded_image)
+
+        elif operation == 'Skin Detector':
+            skin(uploaded_image)
+
+        elif operation == 'Edge Detector':
+            edge(uploaded_image)
+
+        elif operation == 'Resize':
+            resize(uploaded_image)
+
+        elif operation == 'Original':
+            original(uploaded_image)
+
 
 if __name__ == "__main__":
     main()
