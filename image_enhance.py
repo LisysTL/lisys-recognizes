9c74c1476f0885a69f6deeb5178aa7fb15f96406
Devansh <devansh.lisys@gmail.com>
2024-03-22 12:47:35 +0530

    Updated Functions

--- image_enhance.py
+++ image_enhance.py
@@ -1,23 +1,20 @@
-# image_enhance.py
-import numpy as np
+# Classroom Image Enhancer
+"""
+How to use :-
+1. call the Image Enhancer function from the file.
+Example :- import image_enhance as ie
+
+2. Apply Function on images.
+Example:- result = ie.Image_Enhancer(image_path)
+
+3. The Function automatically reads the image and converts it into grayscale for Enhancement.
+
+4. Do not use this Function on Detected Images.
+"""
+
 import cv2
+import numpy as np
 from skimage import exposure
-
-
-def original(image):
-    return image
-
-
-def brightness(image): # use when Image Too Dark
-    enhanced_image = cv2.equalizeHist(image)
-    print('Image enhanced')
-    return enhanced_image
-
-
-def contrast(image):  # Use when Image Too Bright
-    p2, p98 = np.percentile(image, (2, 98))
-    image_contrast_stretched = exposure.rescale_intensity(image, in_range=(p2, p98))
-    return image_contrast_stretched
 
 
 def gamma_correction(image, threshold=127):  # Automatically brightens or darkens the image
@@ -31,7 +28,7 @@ def gamma_correction(image, threshold=127):  # Automatically brightens or darken
     return image_gamma_corrected
 
 
-def reduce_motion_blur(image, kernel_size=(3, 3), blur_strength=5):  # Reduces Motion Blur, Increases Sharpness
+def reduce_motion_blur(image, kernel_size=(2, 2), blur_strength=2):  # Reduces Motion Blur, Increases Sharpness
     # Create motion blur kernel
     kernel = np.zeros(kernel_size)
     kernel[int((kernel_size[0] - 1) / 2), :] = np.ones(kernel_size[0])
@@ -48,51 +45,11 @@ def reduce_motion_blur(image, kernel_size=(3, 3), blur_strength=5):  # Reduces M
     return deconvolved
 
 
-def edge_detector(image):  # Canny Edge Detector
-    edges = cv2.Canny(image, threshold1=100, threshold2=200)
-    return edges
+def Image_Enhancer(image):  # Auto Enhancer
 
+    image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)  # Reading and Grayscale Conversion
+    image = cv2.resize(image, (1550, 850))  # Resizing
 
-def skin_detection(image):  # Segments Skin from Images
-    # Convert image to YCrCb color space
-    ycrcb = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2YCrCb)
-    # Define lower and upper bounds for skin color in YCrCb color space
-    lower_skin = np.array([0, 133, 77], dtype=np.uint8)
-    upper_skin = np.array([255, 173, 127], dtype=np.uint8)
-
-    # Create a mask using the defined skin color range
-    mask = cv2.inRange(ycrcb, lower_skin, upper_skin)
-
-    # Apply morphological operations to remove noise
-    kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.erode(mask, kernel, iterations=2)
-    mask = cv2.dilate(mask, kernel, iterations=2)
-
-    # Increase pixel values of skin regions
-    result = cv2.bitwise_and(image, image, mask=mask)
-    return result
-
-
-def image_resizer(image):  # Resizes Image before enhancement
-    width = 1600
-    height = 900
-
-    image = cv2.resize(image, (width, height))
-    return image
-
-
-def Image_Enchancer(image, threshold=127):  # Auto Enchancer
-
-    # Calculate the average pixel intensity
-    avg_intensity = cv2.mean(image)[0]
-
-    # Check if the average intensity is above the threshold
-    if avg_intensity > threshold:
-        Enchanced_Image = contrast(image)
-
-    else:
-        Enchanced_Image = brightness(image)
-
-    # Increasing Sharpness
-    Enchanced_Image = reduce_motion_blur(image)
-    return Enchanced_Image
+    Enhanced_Image = gamma_correction(image)  # Brightness and Contrast
+    Enhanced_Image = reduce_motion_blur(Enhanced_Image)  # Blur and Sharpen
+    return Enhanced_Image
